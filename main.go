cf8237ce320707264f1562abb07b2f6ad4339cdb
prana <pranadhika22@gmail.com>
2025-04-26 10:20:58 +0700

    feat: remove all logic on main func

--- main.go
+++ main.go
@@ -2,15 +2,8 @@ package main
 
 import (
 	"fmt"
-	"leetcode-go/leetcode"
 )
 
 func main() {
 	fmt.Println("Hello, World Leetcode!")
-
-	nums := []int{2, 7, 11, 15}
-	target := 9
-
-	result := leetcode.TwoSum(nums, target)
-	fmt.Println("result: ", result)
 }
