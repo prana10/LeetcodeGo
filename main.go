753bd26d305a256d8bd30c8b8653793fdd95368a
prana <pranadhika22@gmail.com>
2025-04-26 10:45:23 +0700

    feat: remove logic handle on main func

--- main.go
+++ main.go
@@ -2,25 +2,8 @@ package main
 
 import (
 	"fmt"
-	"leetcode-go/leetcode"
 )
 
 func main() {
 	fmt.Println("Hello, World Leetcode!")
-	// Contoh input pertama: l1 = [2,4,3], l2 = [5,6,4]
-	l1 := &leetcode.ListNode{Val: 2, Next: &leetcode.ListNode{Val: 4, Next: &leetcode.ListNode{Val: 3}}}
-	l2 := &leetcode.ListNode{Val: 5, Next: &leetcode.ListNode{Val: 6, Next: &leetcode.ListNode{Val: 4}}}
-
-	result := leetcode.AddTwoNumbers(l1, l2)
-
-	// Cetak hasil
-	fmt.Print("Hasil: [")
-	for result != nil {
-		fmt.Print(result.Val)
-		result = result.Next
-		if result != nil {
-			fmt.Print(",")
-		}
-	}
-	fmt.Println("]")
 }
