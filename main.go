6d183d8dceb72bd370187283c769b4e56649b7df
prana <pranadhika22@gmail.com>
2025-04-28 13:33:55 +0700

    feat: clean main func

--- main.go
+++ main.go
@@ -2,18 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"leetcode-go/leetcode"
 )
 
 func main() {
 	fmt.Println("Hello, World Leetcode!")
-
-	example1 := "abcabcbb"
-	example2 := "bbbbb"
-	example3 := "pwwkew"
-
-	fmt.Println(leetcode.LengthOfLongestSubstring(example1))
-	fmt.Println(leetcode.LengthOfLongestSubstring(example2))
-	fmt.Println(leetcode.LengthOfLongestSubstring(example3))
 }
